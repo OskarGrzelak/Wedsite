ea70152c5c3f9ca8002d1263067459c751a7bd5f
OskarGrzelak <oskargrzelak@gmail.com>
2019-07-03 14:43:13 +0200

    info section

---
+++ src/Components/Info/Info.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './Info.module.css';
+
+const info = () => (
+    <div className={styles.Info}>
+        <div>
+            <p>Co się dzieje?</p>
+            <p>Asia i Oskar pobierają się</p>
+        </div>
+        <div>
+            <p>Kiedy?</p>
+            <p>12 września 2020</p>
+        </div>
+        <div>
+            <p>Gdzie?</p>
+            <p><span>Hotel Junior,</span><span>ul. Skotnicka 272, Kraków</span></p>
+        </div>
+    </div>
+);
+
+export default info;
